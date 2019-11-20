0bb9e5f1fe4f0d8da584cac95ed2a19c48835f1d
lilyChang409 <55813002+lilyChang409@users.noreply.github.com>
2019-11-20 10:45:52 -0800

    Add files via upload

---
+++ convertor.java
@@ -0,0 +1,15 @@
+
+public class convertor
+{
+   public static void main(int n){
+     String new1="";
+     while(n>0){
+         new1=n%2+new1;
+         n=n/2;
+        }
+      System.out.println(new1);
+    }
+    public static void number(String arg[]){
+     main(4);   
+    }
+}
