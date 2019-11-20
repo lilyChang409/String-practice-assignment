6134f6c6c7ed1e43f46b277ff4fc08053c2f6ec0
lilyChang409 <55813002+lilyChang409@users.noreply.github.com>
2019-11-20 10:33:20 -0800

    Add files via upload

---
+++ palindrome.java
@@ -0,0 +1,25 @@
+
+public class palindrome
+{
+     public static void check (String[] args) {
+        String a = "Amy, must I jujitsu my ma?";
+         System.out.println(palindrome(a));
+    }
+    public static boolean palindrome(String str) {
+        str = str.toLowerCase();
+        String new1 = "";
+        String back2="";
+        for (int i=0;i< str.length();i++) {
+            char letter = str.charAt(i);
+            if (Character.isLetter(letter))
+                new1 += letter;
+        }
+        for (int j= 0; j < new1.length();j++) {
+            char front = new1.charAt(j);
+            char back  = new1.charAt(new1.length() - 1 - j);
+            if (front != back) return false;
+        }
+        return true;
+       
+   
+}}
